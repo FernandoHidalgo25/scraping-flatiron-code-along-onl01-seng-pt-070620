cc9c059d687d1fb8cf7d339421cf6eadd1e7ad9a
Fernando Hidalgo <fernandojesus.hidalgominaya9@gmail.com>
2020-08-11 03:53:49 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,8 +5,8 @@ require_relative './course.rb'
 
 class Scraper
   
-   def get_page
-      # more code coming soon!
+  def get_page
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
   end
   
   def print_courses
